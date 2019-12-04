b9c99e54194da31a0aa68571515a3891c48bc2cb
zbo <zbo2@qq.com>
2019-12-04 20:28:47 +0800

    Test B and D

---
+++ zbogtest/testD.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "../TestPluginTCale/Common.h"
+#include "../TestPluginTCale/TCalcFuncSets.h"
+#include "../TestPluginTCale/SearchApp.h"
+#include "../TestPluginTCale/FindApp.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+TEST(Sample, StringToFloat) {
+	float a = atof("1.1");
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
