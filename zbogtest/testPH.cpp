9a87237412c7a69dceb79f093a0d8441b3ff61b5
zbo <zbo2@qq.com>
2019-12-07 13:34:53 +0800

    POHuai

---
+++ zbogtest/testPH.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "../TestPluginTCale/Common.h"
+#include "../TestPluginTCale/TCalcFuncSets.h"
+#include "../TestPluginTCale/SearchApp.h"
+#include "../TestPluginTCale/FindApp.h"
+#include "../TestPluginTCale/BI.h"
+#include "../TestPluginTCale/FX.h"
+#include "../TestPluginTCale/DUAN.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+vector<FXing*> LoadFXPH(vector<KXian*>& KXianVector)
+{
+	vector<FXing*> FXVector = Find_All_FX(KXianVector);
+	FXVector = AdjustGapInFX(FXVector);
+	FXSearchResult result1 = Find_First_FX_FromALL(FXVector);
+	vector<int> final = Generate_Final_Index(result1, FXVector);
+	vector<FXing*> Final_FXVector = Generate_Final_FX(final, FXVector);
+	return Final_FXVector;
+}
+
+TEST(ALL_POHUAI, 002947_PH_DUAN) {
+	TestDataBag* bag = LoadData("..\\testdata\\002947.txt");
+	vector<KXian*> KXianVector = GenerateKXianVector(bag->DataLength, bag->out, bag->pfINa, bag->pfINb, bag->pfINc);
+	vector<FXing*> Final_FXVector = LoadFXPH(KXianVector);
+	vector<BI*> BIVector = GenerateBIVector(Final_FXVector);
+	EXPECT_EQ(10, BIVector.size());
+}
